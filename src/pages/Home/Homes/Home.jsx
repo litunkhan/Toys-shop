a4dbd40207b17f3a787aad158b3a26d1f5cbd526
litunkhan <litunkhan6@gmail.com>
2023-05-18 17:08:30 +0600

    ourclient

--- src/pages/Home/Homes/Home.jsx
+++ src/pages/Home/Homes/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "../banner/Banner";
+import Clients from "../clients/Clients";
 
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Clients></Clients>
         </div>
     );
 };
