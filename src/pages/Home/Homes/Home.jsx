368bf0a9f89a036279eef9e01f8392bb157068d4
litunkhan <litunkhan6@gmail.com>
2023-05-19 21:35:01 +0600

    addaos

--- src/pages/Home/Homes/Home.jsx
+++ src/pages/Home/Homes/Home.jsx
@@ -1,18 +1,24 @@
+import { useEffect } from "react";
 import Gallery from "../Gellary/Gallery";
 import Banner from "../banner/Banner";
 import Clients from "../clients/Clients";
 import Offsell from "../offSell/Offsell";
 import Reacttabs from "../tabs/Reacttabs";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 
 const Home = () => {
+    useEffect(() => {
+        AOS.init({duration:2000});
+      }, [])
     return (
         <div>
-            <Banner></Banner>
-            <Gallery></Gallery>
+            <div data-aos="zoom-in-right" ><Banner></Banner></div> 
+           <div data-aos="zoom-in"><Gallery></Gallery></div> 
             <Reacttabs></Reacttabs>
             <Clients></Clients>
-            <Offsell></Offsell>
+           <Offsell></Offsell>
         </div>
     );
 };
